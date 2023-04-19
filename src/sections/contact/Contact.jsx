d1573124bea56479c71e59a8684ef7537844b4fe
jackson-ribeiro <jacksonquest1@gmail.com>
2023-04-18 23:01:30 -0300

    style: sections definidas

---
+++ src/sections/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import './contact.css'
+
+const Contact = () => {
+  return (
+    <div>
+      <section id="contact"><h2>Contact</h2></section>
+    </div>
+  )
+}
+
+export default Contact
---
+++ src/sections/faqs/FAQs.jsx
@@ -0,0 +1,9 @@
+import './FAQs.css'
+
+const FAQs = () => {
+  return (
+    <section id="faqs"><h2>FAQs</h2></section>
+  )
+}
+
+export default FAQs
