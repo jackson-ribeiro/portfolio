16bda4959c23292aa458f4ebbeebae69b193beed
Jackson Ribeiro de Oliveira Filho <jacksonfilho@deso-se.com.br>
2023-04-19 16:16:06 -0300

    feat: realizando integrações

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,8 +1,23 @@
+import AboutImage from '../../assets/about.jpg';
 import './about.css';
 
 const About = ()  => {
   return (
-    <section id="about"><h2>About</h2></section>
+    <section id="about">
+      <div className="container about_container">
+        <div className="about_left">
+          <div className='about_portrait'>
+            <img src = {AboutImage} alt = "" />
+          </div>
+          <div className="about_right">
+            <h2>Sobre Mim</h2>
+            <div className="about_cards">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
