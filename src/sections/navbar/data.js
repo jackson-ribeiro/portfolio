e5ace1faea70fac72761ed5d99a79b47651167e9
jackson-ribeiro <jacksonquest1@gmail.com>
2023-04-19 01:47:40 -0300

    style: navbar finalizada

---
+++ src/sections/navbar/data.js
@@ -0,0 +1,9 @@
+const data = [
+    {id: 1, link: '#', title: 'Início'},
+    {id: 2, link: '#', title: 'Sobre'},
+    {id: 3, link: '#', title: 'Serviços'},
+    {id: 4, link: '#', title: 'Portfolio'},
+    {id: 5, link: '#', title: 'Contato'}
+]
+
+export default data;
